1fb0a47013050fdeb1c7d8088922a75d73723e66
syltr1x <syltr1x@proton.me>
2024-03-30 00:51:16 -0300

    add: send and receive messages (beta)

--- src/views/ChatView.jsx
+++ src/views/ChatView.jsx
@@ -1,42 +1,98 @@
-import {React, useState } from 'react';
-import { Text, View, TextInput, StyleSheet, Button, Alert } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, StyleSheet, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { ChatHeader } from "../components/Header";
+import { io } from 'socket.io-client';
 
 const ChatView = () => {
   const [inputValue, setInputValue] = useState('');
-  const handleInputChange = (text) => {setInputValue(text)}
-  const handleSubmit = () => {Alert.alert('Valor ingresado:', inputValue)}
-  
-  return (
-    <View style={{flexGrow: 1, backgroundColor: '#111'}}>
-        <ChatHeader></ChatHeader>
-        <Text style={{color: '#fff'}}>ESTO ES CHAT</Text>
-        <TextInput 
-        style={ChatStyles.msgentry}
-        placeholder="HOLA"
-        onChangeText={handleInputChange}
-        ></TextInput>
-        <Button
-        title="Send Message"
-        onPress={handleSubmit}
-        ></Button>
-      </View>
-  )
-}
+  const [messages, setMessages] = useState(["hola", "koko"]); // Estado para almacenar los mensajes
 
+  const handleInputChange = (text) => {
+    setInputValue(text);
+  };
+
+  const handleSubmit = () => {
+    const socket = io.connect('http://192.168.0.26:3000');
+    socket.emit('message', { user: "martin", msg: inputValue });
+    setInputValue('');
+  };
+
+  useEffect(() => {
+    const socket = io.connect('http://192.168.0.26:3000');
+    socket.on('message', (data) => {
+      setMessages(prevMessages => [...prevMessages, `[${data.user}] > ${data.msg}`]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <View style={{ flexGrow: 1, backgroundColor: '#111' }}>
+      {/* Contenedor de mensajes */}
+      <ChatHeader />
+      <View style={ChatStyles.chatbox}>
+        {messages.map((message, index) => (
+          <View key={index} style={ChatStyles.msgbox}>
+            <Text>{message}</Text>
+          </View>
+        ))}
+      </View>
+      
+      {/* Entrada de mensaje */}
+      <View style={{ flexDirection: 'row' }}>
+        <TextInput
+          style={ChatStyles.msgentry}
+          placeholder="HOLA"
+          onChangeText={handleInputChange}
+          value={inputValue} // Asignar el valor del input
+        />
+        <Icon.Button
+          style={ChatStyles.sendbtn}
+          name="arrow-redo-outline"
+          onPress={handleSubmit}
+        />
+      </View>
+    </View>
+  );
+};
 
 const ChatStyles = StyleSheet.create({
-  msgentry:{
+  chatbox: {
     padding: 12,
     paddingTop: 4,
     paddingBottom: 4,
-    color:'#fff',
-    backgroundColor:'#666',
+    marginBottom: 8,
+    height: '70%',
+    color: '#fff',
+    backgroundColor: '#666',
+    borderWidth: 2,
+    borderRadius: 10,
+  },
+  msgbox: {
+    backgroundColor: '#050',
+    borderRadius: 4,
+    marginBottom: 6,
+    padding: 8
+  },
+  msgentry: {
+    flex: 1,
+    padding: 12,
+    paddingTop: 4,
+    paddingBottom: 4,
+    color: '#fff',
+    backgroundColor: '#666',
     borderWidth: 2,
     borderRadius: 2,
     borderColor: '#f00',
     borderStyle: 'solid'
+  },
+  sendbtn: {
+    padding: 8,
+    borderRadius: 1
   }
-})
+});
 
-export default ChatView
+export default ChatView;
